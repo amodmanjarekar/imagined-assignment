6f793257926fcce5e11bae166a96b85a470aacb5
Amod Manjarekar <amodm.1102@gmail.com>
2024-12-04 22:02:49 +0530

    Build router & controller for products

---
+++ src/routes/productRouter.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const productRouter = express.Router();
+import * as productController from "../controllers/productController";
+
+productRouter.route("/create").post(productController.createProduct);
+
+productRouter.route("/delete/:id").post(productController.deleteProduct);
+
+productRouter.route("/").get(productController.getAllProducts);
+
+productRouter
+  .route("/:id")
+  .get(productController.getProduct)
+  .patch(productController.updateProduct);
+
+export default productRouter;
