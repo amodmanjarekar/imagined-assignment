dbcb780f80351bafdae0bc456df95fe66568236e
Amod Manjarekar <amodm.1102@gmail.com>
2024-12-04 22:16:49 +0530

    Add product routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 import userRouter from "./routes/userRouter";
+import productRouter from "./routes/productRouter";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
 
 app.use(express.json());
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
