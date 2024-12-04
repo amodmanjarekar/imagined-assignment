36591140d781ea591ecdba4e960b545230825e89
Amod Manjarekar <amodm.1102@gmail.com>
2024-12-04 21:46:42 +0530

    Fix Product Model

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: String,
   category: String,
-  price: {type: Number, required: true},
-  stock: {type: Number, required: true, default: 0},
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true, default: 0 },
 });
+
+export const ProductModel = mongoose.model("Product", productSchema);
